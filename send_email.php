5400c3f7525e9846b305e9cbe397bcacffc170e1
Aryan Mishra <118302069+hola-aryan@users.noreply.github.com>
2023-01-20 12:46:17 +0530

    privacy

--- send_email.php
+++ send_email.php
@@ -15,12 +15,12 @@ $mail = new PHPMailer(true);
 try {
     //Server settings                 //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Host       = '';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail -> Username = 'bhartiyarailway808@gmail.com';
-    $mail -> Password = 'oinqdptbmvdacemy';                             //SMTP password
-    $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
-    $mail->Port       = 465;             //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+    $mail -> Username = '';
+    $mail -> Password = '';                             //SMTP password
+    $mail->SMTPSecure = '';            //Enable implicit TLS encryption
+    $mail->Port       = ;             //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
     $mail->setFrom('bhartiyarailway808@gmail.com');
